6a5cbbd5ad31b92b03074481018cc88f72be4e22
4amshin <shinmendenz@gmail.com>
2024-09-05 13:09:16 +0800

    Create Product API

--- app/Http/Controllers/Api/ProductApiController.php
+++ app/Http/Controllers/Api/ProductApiController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Http\Requests\StoreProductRequest;
 use App\Http\Resources\ProductResource;
 use App\Models\Product;
 use Illuminate\Http\Request;
@@ -21,9 +22,15 @@ class ProductApiController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(StoreProductRequest $request)
     {
-        //
+        $validatedData = $request->validated();
+
+        if($validatedData) {
+            $product = Product::create($validatedData);
+
+            return new ProductResource($product);
+        }
     }
 
     /**
